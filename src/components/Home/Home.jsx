64ee5a7ee50f333805a1ebc4edced10bdf5144c7
SamSam Hashi <sammymadobe@gmail.com>
2025-09-27 14:27:46 -0500

    Feat: add round Quick Links (GitHub, LinkedIn, Resume) with hover effects

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,3 +1,4 @@
+import { FaGithub, FaLinkedin, FaFilePdf } from "react-icons/fa";
 import profilepic from "../../assets/profilepic.jpg";
 import "./Home.css";
 
@@ -5,11 +6,15 @@ function Home({ onViewMyWork, onGetInContact }) {
   return (
     <main className="home">
       <header>
-        <img src={profilepic} alt="Profile" className="home__profile-image" />
+        <img
+          src={profilepic}
+          alt="Portrait of SamSam"
+          className="home__profile-image"
+        />
         <h1 className="home__name">Hi, I'm SamSam</h1>
         <p className="home__description">
-          A passionate detail-oriented Full-stack developer with a passion for
-          helping others.
+          A detail-oriented Full-stack developer driven by building meaningful
+          solutions that help others.
         </p>
         <div className="home__buttons">
           <button className="home__button" onClick={onViewMyWork}>
@@ -18,6 +23,25 @@ function Home({ onViewMyWork, onGetInContact }) {
           <button className="home__button" onClick={onGetInContact}>
             Get In Contact
           </button>
+        </div>
+        <div className="home__links">
+          <a
+            href="https://github.com/Samjamhas10"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaGithub size={24} />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/samsamjamahashi/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaLinkedin size={24} />
+          </a>
+          <a href="/resume.pdf" target="_blank" rel="noopener noreferrer">
+            <FaFilePdf size={24} />
+          </a>
         </div>
       </header>
     </main>
