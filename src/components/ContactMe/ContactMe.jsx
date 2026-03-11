f154a8f5628d6801ae33e24bed238c1adb3c0b65
SamSam Hashi <sammymadobe@gmail.com>
2026-03-11 11:15:00 -0500

    Update ContactMe component styles and structure for responsiveness and improved design

--- src/components/ContactMe/ContactMe.jsx
+++ src/components/ContactMe/ContactMe.jsx
@@ -1,11 +1,46 @@
 import "./ContactMe.css";
+import {
+  FaGithub,
+  FaLinkedin,
+  FaEnvelope,
+  FaMapMarkerAlt,
+} from "react-icons/fa";
 
 function ContactMe() {
   return (
     <div className="contactme">
+      <h2>Contact Me</h2>
+
+      <p className="intro">
+        I'm always open to discussing new opportunities, projects, or
+        collaborations.
+      </p>
       <p>
-        You can reach me at:{" "}
+        <FaEnvelope className="icon" />{" "}
         <a href="mailto:sjama9660@gmail.com">sjama9660@gmail.com</a>
+      </p>
+      <p>
+        <FaLinkedin className="icon" />{" "}
+        <a
+          href="https://linkedin.com/in/your-linkedin"
+          target="_blank"
+          rel="noreferrer"
+        >
+          linkedin.com/in/your-linkedin
+        </a>
+      </p>
+      <p>
+        <FaGithub className="icon" />{" "}
+        <a
+          href="https://github.com/Samjamhas10"
+          target="_blank"
+          rel="noreferrer"
+        >
+          github.com/Samjamhas10
+        </a>
+      </p>
+      <p>
+        <FaMapMarkerAlt className="icon" /> Saint Paul, MN
       </p>
     </div>
   );
